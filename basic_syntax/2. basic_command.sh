350b193a9e3a00e4830b62ebba195c67620e2170
sueroku <eraltndusn@gmail.com>
2024-05-29 15:16:55 +0900

    all

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -10,15 +10,20 @@ git add . # 모든 수정, 추가 사항 add
 # local repository에 반영 및 커밋 이력 생성
 git commit -m "메세지타이틀" -m "메세지내용"
 
-# vi 모드에서 작성, git commit 후 엔터
+# vi 모드에서 작성, git commit 후 엔터 첫줄 타이틀 다음줄 내용
 # 수연님.. 보고싶어요..... 
 
 # add와 commit 동시에
 git commit -am "메세지"
 
-
-
-
-
 # commit 이력 확인 (local repository의 이력)
 git log
+# git log 간결하게 
+git log --oneline
+# 전체 이력 조회
+git log --all
+
+# 원격저장소로 업로드
+git push origin 브랜치명
+
+# 충돌 무시하고 강제 push
